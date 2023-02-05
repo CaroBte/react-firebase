d27e4cdf65a33ecc1181054f6c4bdf0cb64615d7
CaroBte <karito0721@hotmail.com>
2023-02-05 13:47:20 -0500

    feat: added firebase

---
+++ src/api/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBkBMNNJ0Kot6XS1wnvmY4hyRBzID0B7bY",
+    authDomain: "deploys-bootcamp-df68e.firebaseapp.com",
+    projectId: "deploys-bootcamp-df68e",
+    storageBucket: "deploys-bootcamp-df68e.appspot.com",
+    messagingSenderId: "148848576163",
+    appId: "1:148848576163:web:b7f578a11a727350576116"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
--- src/api/index.js
+++ src/api/index.js
@@ -1,3 +1,4 @@
 import * as users from "./users"
+import * as firebase from "./firebase"
 
 export { users }
